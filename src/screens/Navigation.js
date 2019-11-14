5ddf24bac750e739252f4ce70e18b6502dee6f67
Alejandro Leal <aleaberm12@gmail.com>
2019-11-14 13:53:05 +0100

    mejoras en navegación

--- src/screens/Navigation.js
+++ src/screens/Navigation.js
@@ -1,53 +1,90 @@
+import React, { PureComponent } from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createStackNavigator } from 'react-navigation-stack';
+import { enableScreens } from 'react-native-screens';
+import { connect } from 'react-redux';
 
 import Tutorial from './Tutorial';
 import Home from './Home';
 import Settings from './Settings';
 import Offline from './Offline';
 
-// Navegación de la app
-const AppStack = createDrawerNavigator({
-    Home: {
-        screen: Home
-    },
-    Settings: {
-        screen: Settings
-    }
-},
+class Navigation extends PureComponent
 {
-    initialRouteName: 'Home'
-});
-
-// Navegación del tutorial
-const TutorialStack = createStackNavigator(
-{ 
-    Tutorial: Tutorial 
-},
-{
-    headerMode: 'none'
-});
-
-// Navegación offline
-const OfflineStack = createStackNavigator(
-{ 
-    Offline: Offline 
-}, 
-{
-    headerMode: 'none'
-});
-
-// Zonas de la app
-const MySwitchNavigator = createSwitchNavigator(
+    configNavigation()
     {
-        Tutorial: TutorialStack,
-        App: AppStack,
-        Offline: OfflineStack,  
-    },
-    {
-        initialRouteName: 'Tutorial',
-    }
-);
+        // Con eso se usará navegación nativa, mejorando el rendimiento
+        enableScreens();
 
-export default Navigation = createAppContainer(MySwitchNavigator);
+        // Navegación de la app
+        const AppStack = createDrawerNavigator({
+            Home: {
+                screen: Home
+            },
+            Settings: {
+                screen: Settings
+            }
+        },
+        {
+            initialRouteName: 'Home'
+        });
+
+        // Navegación del tutorial
+        const TutorialStack = createStackNavigator(
+        { 
+            Tutorial: Tutorial 
+        },
+        {
+            headerMode: 'none'
+        });
+
+        // Navegación offline
+        const OfflineStack = createStackNavigator(
+        { 
+            Offline: Offline 
+        }, 
+        {
+            headerMode: 'none'
+        });
+
+        // Zonas de la app
+        const MySwitchNavigator = createSwitchNavigator(
+            {
+                Tutorial: TutorialStack,
+                App: AppStack,
+                Offline: OfflineStack,  
+            },
+            {
+                initialRouteName: 'Tutorial',
+            }
+        );
+        
+        return createAppContainer(MySwitchNavigator);
+    }
+
+    render() 
+    {
+        const AppContainer = this.configNavigation();
+
+        return (
+            <AppContainer />
+        );
+    }
+}
+ 
+function mapStateToProps(state) 
+{
+    return {
+
+    };
+}
+ 
+function mapDispatchToProps(dispatch) 
+{
+    return {
+
+    };
+}
+ 
+export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
