be3c8415097a14cfb9d77a297a51d9c7f49b3494
Alejandro Leal <aleaberm12@gmail.com>
2020-01-10 12:40:51 +0100

    Se actualiza el readme y la navegación

--- README.md
+++ README.md
@@ -3,21 +3,27 @@
 
 ## Paquetes incluidos
 
-- [Redux](https://es.redux.js.org/)
-- [React Navigation](https://reactnavigation.org/)
+- [Async Storage](https://github.com/react-native-community/async-storage)
+- [React Native NetInfo](https://github.com/react-native-community/react-native-netinfo)
+- [Axios](https://github.com/axios/axios)
+- [React Native Fast Image](https://github.com/DylanVann/react-native-fast-image)
 - [React Native Gesture Handler](https://github.com/kmagiera/react-native-gesture-handler)
 - [React Native Reanimated](https://github.com/kmagiera/react-native-reanimated)
+- [React Native Screens](https://github.com/kmagiera/react-native-screens)
+- [React Native SVG](https://github.com/react-native-community/react-native-svg)
+- [React Navigation](https://reactnavigation.org/)
+- [React Navigation Drawer](https://github.com/react-navigation/drawer)
+- [React Navigation Stack](https://github.com/react-navigation/stack)
+- [React Redux](https://github.com/reduxjs/react-redux)
+- [Redux](https://es.redux.js.org/)
 - [Redux Axios Middleware](https://github.com/svrcekmichal/redux-axios-middleware)
 - [Redux Persist](https://github.com/rt2zz/redux-persist)
-- [Async Storage](https://github.com/react-native-community/async-storage)
-- [Axios](https://github.com/axios/axios)
-- [React Native SVG](https://github.com/react-native-community/react-native-svg)
-- [React Native NetInfo](https://github.com/react-native-community/react-native-netinfo)
 
 ## Herramientas
 
 - [React Native Debugger](https://github.com/jhen0409/react-native-debugger)
 - [SVG to React Native Component](https://www.smooth-code.com/open-source/svgr/playground)
+- [Yoga Layout](https://yogalayout.com/)
 
 ## Comandos:
 > yarn run < comando >
@@ -32,6 +38,7 @@
 - **clean-android:** Borra la caché en android
 - **clean-ios:** Borra la caché en IOS y reinstala los Pod
 - **android:** Abre la app en el emulador de android
-- **ios:** Abre la app en un emulador de IOS sin notch
-- **iosx:** Abre la app en un emulador de IOS con notch
-- **iost:** Abre la app en un emulador de iPad Pro
+- **ios:** Abre la app en un emulador de IOS (iPhone 8)
+- **iosx:** Abre la app en un emulador de IOS (iPhone X)
+- **iosxsmax:** Abre la app en un emulador de IOS (iPhone Xs Max)
+- **iost:** Abre la app en un emulador de IOS (iPad Pro (12.9-inch) (3rd generation))
--- src/screens/Navigation.js
+++ src/screens/Navigation.js
@@ -1,90 +1,54 @@
-import React, { PureComponent } from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createStackNavigator } from 'react-navigation-stack';
 import { enableScreens } from 'react-native-screens';
-import { connect } from 'react-redux';
 
 import Tutorial from './Tutorial';
 import Home from './Home';
 import Settings from './Settings';
 import Offline from './Offline';
 
-class Navigation extends PureComponent
+// Con eso se usará navegación nativa, mejorando el rendimiento
+enableScreens();
+
+// Navegación de la app
+const AppStack = createDrawerNavigator({
+    Home: Home,
+    Settings: Settings
+},
 {
-    configNavigation()
-    {
-        // Con eso se usará navegación nativa, mejorando el rendimiento
-        enableScreens();
+    initialRouteName: 'Home'
+});
 
-        // Navegación de la app
-        const AppStack = createDrawerNavigator({
-            Home: {
-                screen: Home
-            },
-            Settings: {
-                screen: Settings
-            }
-        },
-        {
-            initialRouteName: 'Home'
-        });
-
-        // Navegación del tutorial
-        const TutorialStack = createStackNavigator(
-        { 
-            Tutorial: Tutorial 
-        },
-        {
-            headerMode: 'none'
-        });
-
-        // Navegación offline
-        const OfflineStack = createStackNavigator(
-        { 
-            Offline: Offline 
-        }, 
-        {
-            headerMode: 'none'
-        });
-
-        // Zonas de la app
-        const MySwitchNavigator = createSwitchNavigator(
-            {
-                Tutorial: TutorialStack,
-                App: AppStack,
-                Offline: OfflineStack,  
-            },
-            {
-                initialRouteName: 'Tutorial',
-            }
-        );
-        
-        return createAppContainer(MySwitchNavigator);
-    }
-
-    render() 
-    {
-        const AppContainer = this.configNavigation();
-
-        return (
-            <AppContainer />
-        );
-    }
-}
- 
-function mapStateToProps(state) 
+// Navegación del tutorial
+const TutorialStack = createStackNavigator(
 {
-    return {
-
-    };
-}
- 
-function mapDispatchToProps(dispatch) 
+    Tutorial: Tutorial
+},
 {
-    return {
+    initialRouteName: 'Tutorial',
+    headerMode: 'none'
+});
 
-    };
-}
- 
-export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
+// Navegación offline
+const OfflineStack = createStackNavigator(
+{
+    Offline: Offline
+},
+{
+    initialRouteName: 'Offline',
+    headerMode: 'none'
+});
+
+// Zonas de la app
+const MySwitchNavigator = createSwitchNavigator(
+{
+    Tutorial: TutorialStack,
+    App: AppStack,
+    Offline: OfflineStack,
+},
+{
+    initialRouteName: 'Tutorial',
+});
+
+export default Navigation = createAppContainer(MySwitchNavigator);
